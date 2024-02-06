7396aa857a455e922fb22aa815d5f02db5021571
Khari Jarrett <khari@viam.com>
2024-02-06 15:56:18 -0500

    packaging

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
-#!/bin/sh
-cd `dirname $0`
+#!/usr/bin/env bash
 
-# Create a virtual environment to run our code
-pip install virtualenv
-VENV_NAME="venv"
-PYTHON="$VENV_NAME/bin/python"
+# bash safe mode. look at `set --help` to see what these are doing
+set -euxo pipefail
 
-python3 -m venv $VENV_NAME
-$PYTHON -m pip install -r requirements.txt -U # remove -U if viam-sdk should not be upgraded whenever possible
+cd $(dirname $0)
+source .env
+./setup.sh
 
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
-exec $PYTHON src/main.py $@
+exec $PYTHON -m src.main $@
