b612afcfc153b951d45318ea724535d0a6ea41e4
Khari Jarrett <khari@viam.com>
2024-02-07 13:06:09 -0500

    trying old things for build

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
+cd `dirname $0`
 
-# bash safe mode. look at `set --help` to see what these are doing
-set -euxo pipefail
+# Create a virtual environment to run our code
+pip install virtualenv
+VENV_NAME="venv"
+PYTHON="$VENV_NAME/bin/python"
 
-cd $(dirname $0)
-source .env
-./setup.sh
-
+python3 -m venv $VENV_NAME
+$PYTHON -m pip install -r requirements.txt -U # remove -U if viam-sdk should not be upgraded whenever possible
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
-exec $PYTHON -m src.main $@
+exec $PYTHON src/main.py $@
